fc38162f6406a09bf9312ab914c66d24bba11656
Steve Davis <webebrickit@gmail.com>
2022-11-10 14:35:51 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const https = require('https');
+const fs = require('fs');
+
+
+const app = express();
+const port = 3000;
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+https.createServer(options, app).listen(3030);
+
+
+// main().catch(err => console.log(err));
+
+async function main() {
+
+}
